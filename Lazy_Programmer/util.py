1a2063b7c0e73d6b1a2e799178fbb12b55698248
hnomura@slb.com <hnomura@slb.com>
2018-06-22 16:36:33 +0900

    udemy lecture files

--- Lazy_Programmer/util.py
+++ Lazy_Programmer/util.py
@@ -7,9 +7,9 @@
 import numpy as np
 import pandas as pd
 
-def get_data(limit=None):
+def get_data(file, limit=None):
     print("Reading in and transforming data...")
-    df = pd.read_csv('./../data/mnist/train.csv')
+    df = pd.read_csv(file)
     data = df.as_matrix()
     np.random.shuffle(data)
     X = data[:, 1:] / 255.0 # data is from 0..255
